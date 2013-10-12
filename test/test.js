35e26cac821188d88948963282f5bfd802302fee
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-10-12 10:54:59 -0400

    update tests

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ var path = require('path');
 describe('point', function(){
 	fs.readdirSync('./test/aul.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
-			it('should work',function(){
-				fs.writeFileSync('./test/pointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+			it('should work for '+filePath,function(){
+				fs.writeFileSync('./test/result/pointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
 			});
 		}
 	});
@@ -15,8 +15,8 @@ describe('point', function(){
 describe('multi point', function(){
 	fs.readdirSync('./test/multipointtest.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
-			it('should work',function(){
-				fs.writeFileSync('./test/multiPointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+			it('should work for '+filePath,function(){
+				fs.writeFileSync('./test/result/multiPointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
 			});
 		}
 	});
