2213c9710ed735e54f4d949981c2e1236e019fed
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-10-12 17:34:13 -0400

    lines are working

--- test/test.js
+++ test/test.js
@@ -5,8 +5,26 @@ var path = require('path');
 describe('point', function(){
 	fs.readdirSync('./test/aul.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
-			it('should work for '+filePath,function(){
+			it('should work on '+filePath,function(){
 				fs.writeFileSync('./test/result/pointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+			});
+		}
+	});
+});
+describe('gdb', function(){
+	fs.readdirSync('./test/GRP.gdb').forEach(function(filePath,i){
+		if(path.extname(filePath)==='.gdbtable'){
+			it('should work on '+filePath,function(){
+				fs.writeFileSync('./test/result/GRP'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/GRP.gdb/'+filePath),fs.readFileSync('./test/GRP.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+			});
+		}
+	});
+});
+/*describe('bikes', function(){
+	fs.readdirSync('./test/BikeInventory.gdb').forEach(function(filePath,i){
+		if(path.extname(filePath)==='.gdbtable'){
+			it('should work on '+filePath,function(){
+				fs.writeFileSync('./test/result/bike'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/BikeInventory.gdb/'+filePath),fs.readFileSync('./test/BikeInventory.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
 			});
 		}
 	});
@@ -15,9 +33,10 @@ describe('point', function(){
 describe('multi point', function(){
 	fs.readdirSync('./test/multipointtest.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
-			it('should work for '+filePath,function(){
-				fs.writeFileSync('./test/result/multiPointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+			it('should work on '+filePath,function(){
+				fs.writeFileSync('./test/multiPointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
 			});
 		}
 	});
 });
+*/
