b71da6cea0c66c0fa9769e850912cb87a0e61e08
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-10-16 09:45:44 -0400

    finishing touches

--- test/test.js
+++ test/test.js
@@ -6,7 +6,14 @@ describe('point', function(){
 	fs.readdirSync('./test/aul.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/pointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type  ===  'FeatureCollection'){
+					outFile = './test/result/pointTest'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/pointTest'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
@@ -15,7 +22,14 @@ describe('gdb', function(){
 	fs.readdirSync('./test/GRP.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/GRP'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/GRP.gdb/'+filePath),fs.readFileSync('./test/GRP.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/GRP.gdb/'+filePath),fs.readFileSync('./test/GRP.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type === 'FeatureCollection'){
+					outFile = './test/result/GRP'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/GRP'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
@@ -24,7 +38,14 @@ describe('gdb', function(){
 	fs.readdirSync('./test/BikeInventory.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/bike'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/BikeInventory.gdb/'+filePath),fs.readFileSync('./test/BikeInventory.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/BikeInventory.gdb/'+filePath),fs.readFileSync('./test/BikeInventory.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type === 'FeatureCollection'){
+					outFile = './test/result/bike'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/bike'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
@@ -34,7 +55,14 @@ describe('multi point', function(){
 	fs.readdirSync('./test/multipointtest.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/multiPointTest'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type === 'FeatureCollection'){
+					outFile = './test/result/multiPointTest'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/multiPointTest'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
@@ -43,7 +71,14 @@ describe('ferry and boston', function(){
 	fs.readdirSync('./test/bostonferry.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/bostonferry'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/bostonferry.gdb/'+filePath),fs.readFileSync('./test/bostonferry.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/bostonferry.gdb/'+filePath),fs.readFileSync('./test/bostonferry.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type === 'FeatureCollection'){
+					outFile = './test/result/bostonferry'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/bostonferry'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
@@ -52,7 +87,14 @@ describe('inner ring', function(){
 	fs.readdirSync('./test/innerRing.gdb').forEach(function(filePath,i){
 		if(path.extname(filePath)==='.gdbtable'){
 			it('should work on '+filePath,function(){
-				fs.writeFileSync('./test/result/innerRing'+i+'.json',JSON.stringify(fdb(fs.readFileSync('./test/innerRing.gdb/'+filePath),fs.readFileSync('./test/innerRing.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'))),{encoding:'utf8'});
+				var outFile;
+				var result = fdb(fs.readFileSync('./test/innerRing.gdb/'+filePath),fs.readFileSync('./test/innerRing.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+				if(result.type === 'FeatureCollection'){
+					outFile = './test/result/innerRing'+path.basename(filePath,'.gdbtable')+'.geojson';
+				}else{
+					outFile = './test/result/innerRing'+path.basename(filePath,'.gdbtable')+'.json';
+				}
+				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
 			});
 		}
 	});
