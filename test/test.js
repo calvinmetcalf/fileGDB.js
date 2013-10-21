756f1f3634cd00fe18e76446a829d32a4742976e
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-10-21 12:40:24 -0400

    simplify tests

--- test/test.js
+++ test/test.js
@@ -1,155 +1,24 @@
 var fdb = require('../lib');
 var fs = require('fs');
 var path = require('path');
-
-describe('point', function(){
-	fs.readdirSync('./test/aul.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/aul.gdb/'+filePath),fs.readFileSync('./test/aul.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type  ===  'FeatureCollection'){
-					outFile = './test/result/pointTest'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/pointTest'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
+var assert = require('geojson-assert');
+function testIt(gdbPath){
+	describe(gdbPath, function(){
+		fs.readdirSync(gdbPath).forEach(function(filePath,i){
+			if(path.extname(filePath)==='.gdbtable'){
+				it('should work on '+filePath,function(){
+					var result = fdb(fs.readFileSync(gdbPath+'/'+filePath),fs.readFileSync(gdbPath+'/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
+					JSON.stringify(result);
+				});
+			}
+		});
+		it('should work async on '+gdbPath,function(done){
+			fdb(gdbPath).then(function(l){
+				done(assert(l,true));
+			},done);
+		});
 	});
-	it('should work async on ./test/aul.gdb',function(done){
-		fdb('./test/aul.gdb').then(function(l){
-			fs.writeFileSync('./test/result/pointTestAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-		},done);
-	});
-});
-
-describe('gdb', function(){
-	fs.readdirSync('./test/GRP.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/GRP.gdb/'+filePath),fs.readFileSync('./test/GRP.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/GRP'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/GRP'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-	it('should work async on ./test/GRP.gdb',function(done){
-		fdb('./test/GRP.gdb').then(function(l){
-			fs.writeFileSync('./test/result/GRPAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-		},done);
-	});
-});
-/*describe('bikes', function(){
-	fs.readdirSync('./test/BikeInventory.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/BikeInventory.gdb/'+filePath),fs.readFileSync('./test/BikeInventory.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/bike'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/bike'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-});*/
-
-describe('multi point', function(){
-	fs.readdirSync('./test/multipointtest.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/multipointtest.gdb/'+filePath),fs.readFileSync('./test/multipointtest.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/multiPointTest'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/multiPointTest'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-	it('should work async on ./test/multipointtest.gdb',function(done){
-		fdb('./test/multipointtest.gdb').then(function(l){
-			fs.writeFileSync('./test/result/multiPointTestAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-			
-		},done);
-	});
-});
-describe('ferry and boston', function(){
-	fs.readdirSync('./test/bostonferry.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/bostonferry.gdb/'+filePath),fs.readFileSync('./test/bostonferry.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/bostonferry'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/bostonferry'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-		it('should work async on ./test/bostonferry.gdb',function(done){
-		fdb('./test/bostonferry.gdb').then(function(l){
-			fs.writeFileSync('./test/result/bostonferryAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-		},done);
-	});
-});
-describe('inner ring', function(){
-	fs.readdirSync('./test/innerRing.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/innerRing.gdb/'+filePath),fs.readFileSync('./test/innerRing.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/innerRing'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/innerRing'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-		it('should work async on ./test/innerRing.gdb',function(done){
-		fdb('./test/innerRing.gdb').then(function(l){
-			fs.writeFileSync('./test/result/innerRingAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-		},done);
-	});
-});
-describe('fuel', function(){
-	fs.readdirSync('./test/fuel.gdb').forEach(function(filePath,i){
-		if(path.extname(filePath)==='.gdbtable'){
-			it('should work on '+filePath,function(){
-				var outFile;
-				var result = fdb(fs.readFileSync('./test/fuel.gdb/'+filePath),fs.readFileSync('./test/fuel.gdb/'+path.basename(filePath,'.gdbtable')+'.gdbtablx'));
-				if(result.type === 'FeatureCollection'){
-					outFile = './test/result/fuel'+path.basename(filePath,'.gdbtable')+'.geojson';
-				}else{
-					outFile = './test/result/fuel'+path.basename(filePath,'.gdbtable')+'.json';
-				}
-				fs.writeFileSync(outFile,JSON.stringify(result,false,4),{encoding:'utf8'});
-			});
-		}
-	});
-		it('should work async on ./test/fuel.gdb',function(done){
-		fdb('./test/fuel.gdb').then(function(l){
-			fs.writeFileSync('./test/result/innerRingAsync.json',JSON.stringify(l,false,4),{encoding:'utf8'});
-			done();
-		},done);
-	});
-});
+}
+['./test/aul.gdb','./test/GRP.gdb','./test/multipointtest.gdb','./test/bostonferry.gdb','./test/innerRing.gdb','./test/fuel.gdb'].map(testIt);
+//uncoment if your sure you have enough memory
+//testIt('./test/BikeInventory.gdb');
